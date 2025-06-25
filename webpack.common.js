ba319da5fb28c1b192a977b9d54c23f58c4a4202
Jacob N <177889821+baaramuser123@users.noreply.github.com>
2025-06-25 17:22:43 -0400

    split webpackconfig files

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,4 @@
-// webpack.config.js
+// webpack.common.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -9,10 +9,6 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
-  },
-  devtool: "eval-source-map",
-  devServer: {
-    watchFiles: ["./src/template.html"],
   },
   plugins: [
     new HtmlWebpackPlugin({
